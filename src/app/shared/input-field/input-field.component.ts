c04c691a3c74f048dc85a532e80d9e35b60d8d6d
bruno-ivo <73121467+bruno-ivo@users.noreply.github.com>
2021-11-17 09:34:02 -0300

    ''

--- src/app/shared/input-field/input-field.component.ts
+++ src/app/shared/input-field/input-field.component.ts
@@ -1,21 +1,50 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-input-field',
   templateUrl: './input-field.component.html',
-  styleUrls: ['./input-field.component.css']
+  styleUrls: ['./input-field.component.css'],
 })
-export class InputFieldComponent implements OnInit {
-
+export class InputFieldComponent implements OnInit, ControlValueAccessor {
   @Input() classeCSS: any;
   @Input() id: string = '';
   @Input() label: string = '';
   @Input() type = 'text';
   @Input() control: any;
+  @Input() isReadOnly: any;
 
-  constructor() { }
+  private innerValue: any;
 
-  ngOnInit(): void {
+  get value(){
+    return this.innerValue;
   }
 
+  set value(v: any){
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      this.onChangeCb(v);
+    }
+  }
+
+  constructor() {}
+
+  onChangeCb: (_: any) => void  = () => {};
+  onTouchedCb: (_: any) => void  = () => {};
+
+  writeValue(v: any): void {
+    this.value = v;
+  }
+  registerOnChange(fn: any): void {
+   this.onChangeCb = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouchedCb = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void{
+    this.isReadOnly = isDisabled;
+  }
+
+  ngOnInit(): void {}
 }
